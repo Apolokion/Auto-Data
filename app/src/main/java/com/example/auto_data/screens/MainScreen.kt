ea19d5d4ec19b87ef75db4523bff97ab9e4cdced
Apolokion <apolokion00@gmail.com>
2024-06-12 18:54:53 +0200

    Add: - Car Logos PNG - Small Design Edit in MainScreen.kt

--- app/src/main/java/com/example/auto_data/screens/MainScreen.kt
+++ app/src/main/java/com/example/auto_data/screens/MainScreen.kt
@@ -25,20 +25,58 @@ data class CarCompany(val name: String, val icon: Int)
 
 // List of car companies
 val carCompanies = listOf(
+    CarCompany("Acura", R.drawable.acura),
+    CarCompany("Alfa Romeo", R.drawable.alfa),
     CarCompany("Aston Martin", R.drawable.aston),
     CarCompany("Audi", R.drawable.audi),
-    CarCompany("Alfa Romeo", R.drawable.alfa),
     CarCompany("Bentley", R.drawable.bentley),
     CarCompany("BMW", R.drawable.bmw),
+    CarCompany("Bugatti", R.drawable.bugatti),
+    CarCompany("Cadillac", R.drawable.cadillac),
+    CarCompany("Chevrolet", R.drawable.chevrolet),
+    CarCompany("Chrysler", R.drawable.chrysler),
+    CarCompany("Citroen", R.drawable.citroen),
+    CarCompany("Cupra", R.drawable.cupra),
+    CarCompany("Dodge", R.drawable.dodge),
     CarCompany("Ferrari", R.drawable.ferrari),
-    CarCompany("Ford ", R.drawable.ford),
-    CarCompany("Jaguar ", R.drawable.jaguar),
+    CarCompany("Fiat", R.drawable.fiat),
+    CarCompany("Ford", R.drawable.ford),
+    CarCompany("Gmc", R.drawable.gmc),
+    CarCompany("Honda", R.drawable.honda),
+    CarCompany("Hyundai", R.drawable.hyundai),
+    CarCompany("Infiniti", R.drawable.infiniti),
+    CarCompany("Jaguar", R.drawable.jaguar),
+    CarCompany("Jeep", R.drawable.jeep),
+    CarCompany("Kia", R.drawable.kia),
+    CarCompany("Koenigsegg", R.drawable.koenigsegg),
+    CarCompany("Lamborghini", R.drawable.lamborghini),
+    CarCompany("Lancia", R.drawable.lancia),
     CarCompany("Land Rover", R.drawable.land),
     CarCompany("Lexus", R.drawable.lexus),
-    CarCompany("Maserati ", R.drawable.maserati),
-    CarCompany("Aston Martin", R.drawable.aston),
-    CarCompany("BMW", R.drawable.bmw),
-    CarCompany("Aston Martin", R.drawable.aston),
+    CarCompany("Lotus", R.drawable.lotus),
+    CarCompany("Maserati", R.drawable.maserati),
+    CarCompany("Mazda", R.drawable.mazda),
+    CarCompany("Mclaren", R.drawable.mc_laren),
+    CarCompany("Mercedes", R.drawable.mercedes_benz),
+    CarCompany("Mini", R.drawable.mini),
+    CarCompany("Mitsubishi", R.drawable.mitsubishi),
+    CarCompany("Nissan", R.drawable.nissan),
+    CarCompany("Opel", R.drawable.opel),
+    CarCompany("Pagani", R.drawable.pagani),
+    CarCompany("Peugeot", R.drawable.peugeot),
+    CarCompany("Porsche", R.drawable.porsche),
+    CarCompany("Ram", R.drawable.ram),
+    CarCompany("Renault", R.drawable.renault),
+    CarCompany("Rolls Royce", R.drawable.rolls_royce),
+    CarCompany("Seat", R.drawable.seat),
+    CarCompany("Skoda", R.drawable.skoda),
+    CarCompany("Smart", R.drawable.smart),
+    CarCompany("Subaru", R.drawable.subaru),
+    CarCompany("Suzuki", R.drawable.suzuki),
+    CarCompany("Tesla", R.drawable.tesla),
+    CarCompany("Toyota", R.drawable.toyota),
+    CarCompany("Volkswagen", R.drawable.vw),
+    CarCompany("Volvo", R.drawable.volvo),
     // Add more car companies here
 )
 
@@ -104,7 +142,7 @@ fun MainScreen(navController: NavHostController) {
 @Composable
 fun CarCompanyItem(carCompany: CarCompany, navController: NavHostController, isGrid: Boolean) {
     val borderModifier = Modifier.border(width = 1.dp, color = Color.Black)
-    val paddingModifier = Modifier.padding(vertical = 8.dp, horizontal = 10.dp)
+    val paddingModifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)
     val clickableModifier = Modifier.clickable {
         navController.navigate("car_models/${carCompany.name}")
     }
@@ -122,10 +160,10 @@ fun CarCompanyItem(carCompany: CarCompany, navController: NavHostController, isG
             Image(
                 painter = painterResource(id = carCompany.icon),
                 contentDescription = "Car Icon",
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier.size(55.dp)
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = carCompany.name)
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = carCompany.name, style = MaterialTheme.typography.titleMedium)
         }
     }
 }
