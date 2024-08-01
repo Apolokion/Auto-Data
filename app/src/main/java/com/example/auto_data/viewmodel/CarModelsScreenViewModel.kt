3ea820a13ee76f450db0fbca53287cd001b82a9a
Apolokion <apolokion00@gmail.com>
2024-08-01 19:19:21 +0300

    New: Added some car model images and restructured CarModelsScreen.kt

--- app/src/main/java/com/example/auto_data/viewmodel/CarModelsScreenViewModel.kt
+++ app/src/main/java/com/example/auto_data/viewmodel/CarModelsScreenViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.auto_data.viewmodel
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.auto_data.data.CarModel
 import com.example.auto_data.data.carModelsMap
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+
 
 class CarModelsScreenViewModel : ViewModel() {
 
@@ -13,6 +19,16 @@ class CarModelsScreenViewModel : ViewModel() {
     fun getCarModels(carCompany: String?) {
         carCompany?.let {
             carModels.value = carModelsMap[it] ?: emptyList()
+        }
+    }
+
+    fun observeScrollState(listState: LazyListState) {
+        viewModelScope.launch {
+            snapshotFlow { listState.firstVisibleItemIndex }
+                .distinctUntilChanged()
+                .collect { firstVisibleItemIndex ->
+                    isTopAppBarVisible.value = firstVisibleItemIndex == 0
+                }
         }
     }
 
